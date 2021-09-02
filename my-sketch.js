38044b061daba3b655857fdf07e77e12c7b6879b
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-02 18:34:55 +0530

    Radius & Organic Randomness

--- my-sketch.js
+++ my-sketch.js
@@ -9,12 +9,15 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = []
-    const count = 40
+    const count = 30
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v = count <= 1 ? 0.5 : y / (count - 1)
-        points.push([u, v])
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [u, v],
+        })
       }
     }
     return points
@@ -28,15 +31,18 @@ const sketch = () => {
     context.fillStyle = 'white'
     context.fillRect(0, 0, width, height)
 
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+      const { position, radius } = data
+
+      const [u, v] = position
+
       const x = lerp(margin, width - margin, u)
       const y = lerp(margin, height - margin, v)
 
       context.beginPath()
-      context.arc(x, y, 5, 0, Math.PI * 2, false)
-      context.strokeStyle = 'black'
-      context.lineWidth = 20
-      context.stroke()
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false)
+      context.fillStyle = 'red'
+      context.fill()
     })
   }
 }
