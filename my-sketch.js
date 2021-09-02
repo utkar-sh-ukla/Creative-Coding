45491cb704a56e9ab56a2153005c14b83e7336db
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-02 18:21:41 +0530

    Simple Randomness

--- my-sketch.js
+++ my-sketch.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch')
 const { lerp } = require('canvas-sketch-util/math')
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [2048, 2048],
@@ -8,7 +9,7 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = []
-    const count = 5
+    const count = 40
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1)
@@ -19,7 +20,8 @@ const sketch = () => {
     return points
   }
 
-  const points = createGrid()
+  random.setSeed(512)
+  const points = createGrid().filter(() => random.value() > 0.5)
   const margin = 400
 
   return ({ context, width, height }) => {
@@ -31,9 +33,9 @@ const sketch = () => {
       const y = lerp(margin, height - margin, v)
 
       context.beginPath()
-      context.arc(x, y, 100, 0, Math.PI * 2, false)
+      context.arc(x, y, 5, 0, Math.PI * 2, false)
       context.strokeStyle = 'black'
-      context.lineWidth = 40
+      context.lineWidth = 20
       context.stroke()
     })
   }
