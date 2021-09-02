56208a02b2f0cb1db0ce7dd4bb9f5ba51d5de913
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-02 18:46:53 +0530

    Color Palettes

--- my-sketch.js
+++ my-sketch.js
@@ -1,12 +1,16 @@
 const canvasSketch = require('canvas-sketch')
 const { lerp } = require('canvas-sketch-util/math')
 const random = require('canvas-sketch-util/random')
+const palettes = require('nice-color-palettes')
 
 const settings = {
   dimensions: [2048, 2048],
 }
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(2, 6)
+  const palette = random.shuffle(random.pick(palettes).slice(0, colorCount))
+
   const createGrid = () => {
     const points = []
     const count = 30
@@ -15,6 +19,7 @@ const sketch = () => {
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v = count <= 1 ? 0.5 : y / (count - 1)
         points.push({
+          color: random.pick(palette),
           radius: Math.abs(random.gaussian() * 0.01),
           position: [u, v],
         })
@@ -32,7 +37,7 @@ const sketch = () => {
     context.fillRect(0, 0, width, height)
 
     points.forEach((data) => {
-      const { position, radius } = data
+      const { position, radius, color } = data
 
       const [u, v] = position
 
@@ -41,7 +46,7 @@ const sketch = () => {
 
       context.beginPath()
       context.arc(x, y, radius * width, 0, Math.PI * 2, false)
-      context.fillStyle = 'red'
+      context.fillStyle = color
       context.fill()
     })
   }
