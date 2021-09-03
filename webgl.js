81f863656546d55bbc8f3da4c22500017dac9f70
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-03 23:37:33 +0530

    Easing & Trignometry

--- webgl.js
+++ webgl.js
@@ -7,6 +7,8 @@ require('three/examples/js/controls/OrbitControls')
 const canvasSketch = require('canvas-sketch')
 const random = require('canvas-sketch-util/random')
 const palettes = require('nice-color-palettes')
+const eases = require('eases')
+const BezierEasing = require('bezier-easing')
 
 const settings = {
   dimensions: [512, 512],
@@ -66,6 +68,8 @@ const sketch = ({ context }) => {
   light.position.set(0, 0, 4)
   scene.add(light)
 
+  const easeFn = BezierEasing(0.67, 0.03, 0.29, 0.99)
+
   // draw each frame
   return {
     // Handle resize events here
@@ -96,7 +100,9 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ playhead }) {
-      scene.rotation.z = playhead * (Math.PI * 2)
+      const t = Math.sin(playhead * Math.PI)
+      // scene.rotation.z = eases.expoInOut(t)
+      scene.rotation.z = easeFn(t)
       renderer.render(scene, camera)
     },
     // Dispose of events & renderer for cleaner hot-reloading
