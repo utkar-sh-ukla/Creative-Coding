a40c02ae3adb91079817204483b882294906a36d
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-03 13:56:41 +0530

    Isometric Perspective

--- webgl.js
+++ webgl.js
@@ -5,46 +5,51 @@ global.THREE = require('three')
 require('three/examples/js/controls/OrbitControls')
 
 const canvasSketch = require('canvas-sketch')
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
   context: 'webgl',
+  // Turn on MSAA
+  attributes: { antialias: true },
 }
 
 const sketch = ({ context }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
-    canvas: context.canvas,
+    context,
   })
 
   // WebGL background color
-  renderer.setClearColor('#000', 1)
+  renderer.setClearColor('hsl(0, 0%, 95%)', 1)
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100)
-  camera.position.set(0, 0, -4)
-  camera.lookAt(new THREE.Vector3())
-
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas)
+  const camera = new THREE.OrthographicCamera()
 
   // Setup your scene
   const scene = new THREE.Scene()
 
   // Setup a geometry
-  const geometry = new THREE.SphereGeometry(1, 32, 16)
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
 
   // Setup a material
   const material = new THREE.MeshBasicMaterial({
     color: 'red',
-    wireframe: true,
   })
 
-  // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
+  for (let i = 0; i < 10; i++) {
+    // Setup a mesh with geometry + material
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    )
+    mesh.scale.multiplyScalar(0.1)
+    scene.add(mesh)
+  }
 
   // draw each frame
   return {
@@ -52,17 +57,34 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio)
       renderer.setSize(viewportWidth, viewportHeight, false)
-      camera.aspect = viewportWidth / viewportHeight
+      const aspect = viewportWidth / viewportHeight
+
+      // Ortho zoom
+      const zoom = 2
+
+      // Bounds
+      camera.left = -zoom * aspect
+      camera.right = zoom * aspect
+      camera.top = zoom
+      camera.bottom = -zoom
+
+      // Near/Far
+      camera.near = -100
+      camera.far = 100
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom)
+      camera.lookAt(new THREE.Vector3())
+
+      // Update the camera
       camera.updateProjectionMatrix()
     },
     // Update & render your scene here
     render({ time }) {
-      controls.update()
       renderer.render(scene, camera)
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose()
       renderer.dispose()
     },
   }
