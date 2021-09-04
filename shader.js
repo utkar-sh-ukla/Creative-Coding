bd8e2587e71ee524e7d0e0c55684c23cd8411518
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-04 13:05:44 +0530

    Added Colors in Noise

--- shader.js
+++ shader.js
@@ -17,6 +17,7 @@ const frag = glsl(/*glsl*/ `
   varying vec2 vUv;
 
   #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
   void main () {
     // vec3 colorA = cos(time * 2.0) + vec3(1.0, 0.0, 0.0);
@@ -25,16 +26,18 @@ const frag = glsl(/*glsl*/ `
     vec2 center = vUv - 0.5;
     center.x *= aspect;
 
-    // float dist = length(center);
+    float dist = length(center);
 
-    // float alpha = smoothstep(0.50, 0.25, dist);
+    float alpha = smoothstep(0.25, 0.2475, dist);
 
     // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
     // gl_FragColor = vec4(color, alpha);
 
-    float n = noise(vec3(center * 1.0, time));
+    float n = noise(vec3(center * 0.5, time * 0.25));
 
-    gl_FragColor = vec4(vec3(n), 1.0);
+    vec3 color = hsl2rgb(0.6 + n * 0.2, 0.5, 0.5);
+
+    gl_FragColor = vec4(color, alpha);
   }
 `)
 
