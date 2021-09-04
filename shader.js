b79da6c5425735019d2c63bc8f6c9fba59569405
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-09-04 12:54:27 +0530

    Added Noise

--- shader.js
+++ shader.js
@@ -9,26 +9,32 @@ const settings = {
 }
 
 // Your glsl code
-const frag = glsl(/*glsl*/`
+const frag = glsl(/*glsl*/ `
   precision highp float;
 
   uniform float time;
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
   void main () {
-    vec3 colorA = cos(time * 2.0) + vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 0.5, 0.0);
+    // vec3 colorA = cos(time * 2.0) + vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 0.5, 0.0);
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
 
-    float dist = length(center);
+    // float dist = length(center);
 
-    float alpha = smoothstep(0.50, 0.25, dist);
+    // float alpha = smoothstep(0.50, 0.25, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
-    gl_FragColor = vec4(color, alpha);
+    // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+    // gl_FragColor = vec4(color, alpha);
+
+    float n = noise(vec3(center * 1.0, time));
+
+    gl_FragColor = vec4(vec3(n), 1.0);
   }
 `)
 
@@ -45,7 +51,7 @@ const sketch = ({ gl }) => {
     uniforms: {
       // Expose props from canvas-sketch
       time: ({ time }) => time,
-      aspect: ({width, height}) => width /height 
+      aspect: ({ width, height }) => width / height,
     },
   })
 }
